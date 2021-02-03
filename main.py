298055ec77b4ba2925f18cbdf1814bba210d42c8
Ruhen-Bhuiyan <43878741+Ruhen-Bhuiyan@users.noreply.github.com>
2021-02-03 22:58:34 +0600

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import numpy as np
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 def get_title(index):
+	
 	return df[df.index == index]["title"].values[0]
 def get_index(title):
 	return df[df.title == title]["index"].values[0]

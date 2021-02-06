ff2e3c703dca02efca2b400a88d3de8be3ace079
Ruhen-Bhuiyan <43878741+Ruhen-Bhuiyan@users.noreply.github.com>
2021-02-06 23:51:52 +0600

    Update main.py

--- main.py
+++ main.py
@@ -2,16 +2,14 @@ import pandas as pd
 import numpy as np
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
-def get_title(index):
-	
+def get_title(index):	
 	return df[df.index == index]["title"].values[0]
 def get_index(title):
 	return df[df.title == title]["index"].values[0]
 df = pd.read_csv("F:\\GithubMY\\Movie\\dataset.csv")
 features = []
 for feature in features:
-	df[feature] = df[feature].fillna('')	
-	
+	df[feature] = df[feature].fillna('')		
 def combination(row):
 	try:	
 	except:

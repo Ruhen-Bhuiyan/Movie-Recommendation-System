e51a0000a801aba24393ff5fec2567a4801f43d4
Ruhen-Bhuiyan <43878741+Ruhen-Bhuiyan@users.noreply.github.com>
2021-03-06 21:29:15 +0600

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import pandas as pd
-
 import numpy as np
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
@@ -7,7 +6,6 @@ def get_title(index):
 	return df[df.index == index]["title"].values[0]
 def get_index(title):	
 	return df[df.title == title]["index"].values[0]
-
 df = pd.read_csv("F:\\GithubMY\\Movie\\dataset.csv")
 features = []
 for feature in features:
